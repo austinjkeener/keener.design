b2258c1d0425954456634fb99d5f0f509d471c03
Austin Keener <austingraphicandweb@gmail.com>
2020-12-29 11:24:54 -0500

    updaing the gallery pages to work with javascript

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 console.log('hello');
 
-// I want to add code to my design gallery pages so that the image gallery is responsive. When I click on on of the smaller images it displays the image in the larger image container
+// everytime a subheader image is clicked on, it appends to the header by targeting the id of the header and subheader
