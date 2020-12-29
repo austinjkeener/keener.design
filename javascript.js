75be045b8fbf4135074511c592b94a08e55b4e6b
Austin Keener <austingraphicandweb@gmail.com>
2020-12-29 10:40:31 -0500

    environment setup and pseudocode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,3 @@
 console.log('hello');
+
+// I want to add code to my design gallery pages so that the image gallery is responsive. When I click on on of the smaller images it displays the image in the larger image container
