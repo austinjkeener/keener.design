a4b36fca952735a6813d4de1fca9840ee30354e7
Austin Keener <austingraphicandweb@gmail.com>
2020-12-30 15:36:14 -0500

    starting the redoing of the homepage, navigation and foot

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
-console.log('hello');
+  console.log("Lets work together to make this great...");
+  //click event that will hide the hamburger menu when clicked
+  function hideTheBurger() {
+    //internal variables
+    const nav = $("#nav").css("height", "28%");
+    const hamburger = $("#hamburger").css("display", "none");
+    const patty = $("#patty").css("display", "none");
+    const unorderedlist = $("#unorderedlist").css("display", "block");
+    const listitem = $(".listitem").css("display", "block");
+    const button = $("#button").append("<p>X<p>")
+    //i want to append a giant x that will be used to toggle the state of the drop down menu when clicked
 
-// I want to add code to my design gallery pages so that the image gallery is responsive. When I click on on of the smaller images it displays the image in the larger image container
+    //event listeners
+    document.getElementById("button").addEventListener("click", button);
+    document.getElementById("hamburger").addEventListener("click", hamburger);
+    document.getElementById("patty").addEventListener("click", patty);
+    document
+      .getElementById("unorderedlist")
+      .addEventListener("click", unorderedlist);
+    document.getElementById("nav").addEventListener("click", nav);
+  };
+//Event listeners
